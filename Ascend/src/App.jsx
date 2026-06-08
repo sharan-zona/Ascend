ef1d0efbb332d4c7a96eac8b3f85e98b444fee06
Sharan zona I <169944147+sharan-zona@users.noreply.github.com>
2026-06-08 07:21:25 +0000

    login right pannel

--- Ascend/src/App.jsx
+++ Ascend/src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css';
+import Login from './login';
+
 function App() {
   return (
     <div className="container">
       <div className="left">
         <h1 id='Heading'>Ascend</h1>
         <p id='des'>Every thought recorded is a step upward.</p>
-        <div className="pattern-bg">
-        </div>
+        <div className="pattern-bg"></div>
       </div>
-
       <div className="right">
-       
+        <Login />
       </div>
     </div>
   );
