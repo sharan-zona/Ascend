5fdb687c90f0de0acc584b7ccf34c4552ae75b1b
Sharan zona I <169944147+sharan-zona@users.noreply.github.com>
2026-06-08 05:40:34 +0000

    right panel

--- Ascend/src/App.jsx
+++ Ascend/src/App.jsx
@@ -3,11 +3,14 @@ function App() {
   return (
     <div className="container">
       <div className="left">
-        Left Side
+        <h1 id='Heading'>Ascend</h1>
+        <p id='des'>Every thought recorded is a step upward.</p>
+        <div className="pattern-bg">
+        </div>
       </div>
 
       <div className="right">
-        Right Side
+       
       </div>
     </div>
   );
